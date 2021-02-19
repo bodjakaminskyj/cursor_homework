f056db092103ddc1291700f88bc0bf605a4a36cc
bohdan9812 <73465085+bohdan9812@users.noreply.github.com>
2021-02-19 09:34:46 +0200

    hw_02_1

--- hw_02/hw_02.js
+++ hw_02/hw_02.js
@@ -12,12 +12,12 @@ do {
 
 console.log('%c%s','font: 2.0em/1 Arial; color: gold;',`Введене число M =  ${M}`);
 
-const B = confirm("Чи потрібно пропускати парні числа")
-if (B) console.log('%c%s','font: 1.5em/1 Arial; color: olivedrab;',"Парні числа пропускаються")
+const trueOrFalse = confirm("Чи потрібно пропускати парні числа")
+if (trueOrFalse) console.log('%c%s','font: 1.5em/1 Arial; color: olivedrab;',"Парні числа пропускаються")
 else console.log('%c%s','font: 1.5em/1 Arial; color: crimson;',"Парні числа не пропускаються")
 let summ = 0;
     for(let i = N; i <= M; i++){
-        if (!(B && (i+1)%2)){summ += i; }
+        if (!(trueOrFalse && (i+1)%2)){summ += i; }
     }
 console.log('%c%s','font: 2.0em/1 Arial; color: white; background-color: black',`Сума чисел від N до M =  ${summ}`);
 document.writeln("Сума чисел від N до M = ",summ)
