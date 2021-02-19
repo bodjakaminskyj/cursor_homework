21471f9f1b90cbaec029fcfa4128c1c36eb84aad
bohdan9812 <73465085+bohdan9812@users.noreply.github.com>
2021-02-19 09:16:10 +0200

    hw_02_1

--- hw_02/hw_02.js
+++ hw_02/hw_02.js
@@ -1,16 +1,17 @@
 console.log('%c%s', 'font: 2.5em/1 Arial; color: orangered;', 'HW: "Цикли і Розгалуження"')
-let N =Number(prompt("Введіть число N = "));
-    if (N) {
-    N=Math.ceil(N) 
-    console.log('%c%s','font: 2.0em/1 Arial; color: blue',`Число N = ${N}`)
-    }
-    else N = Number(prompt("Введіть коректне число N = "));
-let M = Number(prompt("Введіть число M = "));
-    if (M) {   
-        M=Math.floor(M) 
-        console.log('%c%s','font: 2.0em/1 Arial; color: gold;',`Число M = ${M}`)
-    }
-    else M = Number(prompt("Введіть коректне число N = "));
+let N;
+do {
+    N = Math.ceil(prompt(`Введіть число N = `));
+} while(Number.isNaN(N));
+
+console.log('%c%s','font: 2.0em/1 Arial; color: blue;',`Введене число N =  ${N}`);
+let M;
+do {
+    M = Math.floor(prompt(`Введіть число M = `));
+} while(Number.isNaN(N));
+
+console.log('%c%s','font: 2.0em/1 Arial; color: gold;',`Введене число M =  ${M}`);
+
 const B = confirm("Чи потрібно пропускати парні числа")
 if (B) console.log('%c%s','font: 1.5em/1 Arial; color: olivedrab;',"Парні числа пропускаються")
 else console.log('%c%s','font: 1.5em/1 Arial; color: crimson;',"Парні числа не пропускаються")
