018294698deb022762ad54e4a8f5640049c81651
bohdan9812 <73465085+bohdan9812@users.noreply.github.com>
2021-02-19 16:39:13 +0200

    hw_02

--- hw_02/hw_02.js
+++ hw_02/hw_02.js
@@ -1,28 +1,28 @@
 console.log('%c%s', 'font: 2.5em/1 Arial; color: orangered;', 'HW: "Цикли і Розгалуження"')
-let N;
+let userFirstInputedNumber;
 do {
-    N = parseInt(prompt(`Введіть число N = `));
-} while(Number.isNaN(N));
+    userFirstInputedNumber = parseInt(prompt(`Введіть число N = `));
+} while(Number.isNaN(userFirstInputedNumber));
 
-console.log('%c%s','font: 2.0em/1 Arial; color: blue;',`Введене число N =  ${N}, ${typeof(N)}`);
-let M;
+console.log('%c%s','font: 2.0em/1 Arial; color: blue;',`Введене число N =  ${userFirstInputedNumber}, ${typeof(userFirstInputedNumber)}`);
+let userSecondInputedNumber;
 do {
-    M = parseInt(prompt(`Введіть число M = `));
-} while(Number.isNaN(M));
+    userSecondInputedNumber = parseInt(prompt(`Введіть число M = `));
+} while(Number.isNaN(userSecondInputedNumber));
 
-console.log('%c%s','font: 2.0em/1 Arial; color: gold;',`Введене число M =  ${M}, ${typeof(M)}`);
+console.log('%c%s','font: 2.0em/1 Arial; color: gold;',`Введене число M =  ${userSecondInputedNumber}, ${typeof(userSecondInputedNumber)}`);
 
-const trueOrFalse = confirm("Чи потрібно пропускати парні числа")
-if (trueOrFalse) console.log('%c%s','font: 1.5em/1 Arial; color: olivedrab;',"Парні числа пропускаються")
+const skipEvenValues = confirm("Чи потрібно пропускати парні числа")
+if (skipEvenValues) console.log('%c%s','font: 1.5em/1 Arial; color: olivedrab;',"Парні числа пропускаються")
 else console.log('%c%s','font: 1.5em/1 Arial; color: crimson;',"Парні числа не пропускаються")
 let summ = 0;
-if (N<M){   
-    for(let i = N; i <= M; i++){
-        if (!(trueOrFalse && (i+1)%2)){summ += i; }
+if (userFirstInputedNumber<userSecondInputedNumber){   
+    for(let i = userFirstInputedNumber; i <= userSecondInputedNumber; i++){
+        if (!(skipEvenValues && (i+1)%2)){summ += i; }
     }
 }
-    else {for(let i = M; i <= N; i++){
-        if (!(trueOrFalse && (i+1)%2)){summ += i; }
+    else {for(let i = userSecondInputedNumber; i <= userFirstInputedNumber; i++){
+        if (!(skipEvenValues && (i+1)%2)){summ += i; }
     }}
 
 
