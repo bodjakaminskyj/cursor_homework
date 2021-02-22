1ad9d77c91e0029561ff2792f58bdd25915f55b1
bohdan9812 <73465085+bohdan9812@users.noreply.github.com>
2021-02-22 23:13:00 +0200

    hw_03

--- hw_03/main.js
+++ hw_03/main.js
@@ -16,14 +16,36 @@ function getDegree(baseNumbers, degreeNumbers){
 }
 
 function getName(userName){
-  userName = String(userName)
-  let result = userName[0].toUpperCase() + userName.slice(1).toLowerCase();
-  return result;
+  userName = String(userName);
+  return userName[0].toUpperCase() + userName.slice(1).toLowerCase();;
+}
+
+function getSumm(summa, tax){
+  const rate = 100;
+  return summa - (summa * tax)/rate;
+}
+
+function getRandomNumber(minNumber, maxNumber){
+  return Math.floor(Math.random()*(maxNumber - minNumber + 1) )+ minNumber;
+
+}
+
+function countLetter( letter, word ){
+  let count = 0;
+  for(let i =0; i<word.length; i++){
+    if(word[i]===letter) count++;
+  }
+  return count;
+}
+
+function deleteDuplicateLetter(letter, word){
+
 }
 
 
-
-
-document.writeln(`Функція №1: ${getMaxDigit(356454357235)}`);
-document.writeln(`Функція №2: ${getDegree(2,5)}`);
-document.writeln(`Функція №3: ${getName('VlADIsLAv')}`);
+document.writeln(`Функція №1: ${getMaxDigit(356454357235)} <br>`);
+document.writeln(`Функція №2: ${getDegree(2,5)}<br>`);
+document.writeln(`Функція №3: ${getName('VlADIsLAv')}<br>`);
+document.writeln(`Функція №4: ${getSumm(1000, 19.5)}<br>`);
+document.writeln(`Функція №5: ${getRandomNumber(1, 10)}<br>`);
+document.writeln(`Функція №6: ${countLetter('a', 'ajmdsmjajmjdmdajdaamajanmjamjammnajdnmjf,cf')}<br>`);
