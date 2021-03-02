e2c39d428f8e74260105ad061bd9881f1652221c
bohdan9812 <73465085+bohdan9812@users.noreply.github.com>
2021-03-02 14:05:57 +0200

    hw_05

--- hw_05/main.js
+++ hw_05/main.js
@@ -5,82 +5,47 @@ function getRandomArray(len = 1, min = 1, max = 1 ){
   }
   return randomArray
 }
-/*
-function getModa(...numbers){
-
-}
-*/
-
-var sum = [0, 1, 2, 3].reduce(function (accumulator, currentValue) {
-  return accumulator + currentValue;
-}, 0);
-console.log(sum)
-
-function getAverage(...numbers){
-  numbers.reduce(function (accumulator, currentValue) {
-    return accumulator + currentValue;
-  }, 0);
-  
-}
-/*
-getMedian(...numbers){
-
+function getAverage(...numbers) {
+  return sum = numbers.reduce((accumulator, curentElrment)=> accumulator + curentElrment,0)/numbers.length
 }
 
-*/
+function getMedian(...numbers){
+  numbers.sort((firstElement, secondElement)=> firstElement - secondElement)
+if(numbers.length % 2 === 0){
+  median = (numbers[(numbers.length/2)-1] + numbers[numbers.length/2])/2;
+} else {
+  median = numbers[(numbers.length - 1)/2];
+}
+return median
+}
+
 function filterEvenNumbers(...numbers){
   return evenNumbers = numbers.filter(x => x % 2)
-  }
-
+}
 
 function countPositiveNumbers(...numbers){
-  let count = 0
-  for (let i = 0; i < numbers.length; i++) {
-    numbers[i] > 0 ? count++ : count
-  }
-  return count;
-
+  positiveNumbers = numbers.filter(x => x > 0)
+  return positiveNumbers.length
 }
 
 function getDividedByFive(...numbers){
   return evenNumbers = numbers.filter(x => x % 5 === 0)
 }
-/*
-getDividedByFive(...numbers){
 
+function divideByThree(word, len) {
+  let arr = []
+  for (let i = 0; i < word.length; i+=len) {
+    arr.push(word.toLowerCase().substring(i,i+len))  
+  }
+  return arr  
 }
-
-replaceBadWords(string){
-
-}
-
-divideByThree(word){
-
-}
-
-generateCombinations(word){
-
-}
-
-*/
 
 document.writeln(`Функція №1: ${getRandomArray(15, 1, 100)} <br>`);
-/*
-document.writeln(`Функція №2: ${getRandomArray(15, 1, 100)} <br>`);
-*/
-document.writeln(`Функція №3: ${getAverage(28, -19, -74, 60, 89, 41, -23, 48, 8, 68, -31, -55, 54, -57, 87)} <br>`);
-/*
-document.writeln(`Функція №4: ${getRandomArray(15, 1, 100)} <br>`);
-*/
-document.writeln(`Функція №5: ${filterEvenNumbers(28, -19, -74, 60, 89, 41, -23, 48, 8, 68, -31, -55, 54, -57, 87)} <br>`);
+document.writeln(`Функція №3: ${getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2)} <br>`);
+document.writeln(`Функція №4: ${getMedian(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2)} <br>`);
+document.writeln(`Функція №5: ${filterEvenNumbers(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2)} <br>`);
+document.writeln(`Функція №6: ${countPositiveNumbers(6, 2, 55, -11, -78, 2, -55, 77, -57, -87, 23, 2, 56, 3, 2)} <br>`);
+document.writeln(`Функція №7: ${getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2)} <br>`);
+document.writeln(`Функція №9: ${divideByThree("Commander",3)} <br>`);
 
-document.writeln(`Функція №6: ${countPositiveNumbers(28, -19, -74, 60, 89, 41, -23, 48, 8, 68, -31, -55, 54, -57, 87)} <br>`);
 
-document.writeln(`Функція №7: ${getDividedByFive(28, -19, -74, 60, 89, 41, -23, 48, 8, 68, -31, -55, 54, -57, 87)} <br>`);
-/*
-document.writeln(`Функція №8: ${getRandomArray(15, 1, 100)} <br>`);
-
-document.writeln(`Функція №9: ${getRandomArray(15, 1, 100)} <br>`);
-
-document.writeln(`Функція №10: ${getRandomArray(15, 1, 100)} <br>`);
-*/
