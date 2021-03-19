0a69f15ab4e52e1153a63f40f773dd7efc4f64f9
bohdan9812 <73465085+bohdan9812@users.noreply.github.com>
2021-03-19 21:25:28 +0200

    hw_11_update

--- hw_11/main.js
+++ hw_11/main.js
@@ -1,35 +1,24 @@
-birdSound = document.querySelector('.bird');
-catSound = document.querySelector('.cat');
-dogSound = document.querySelector('.dog');
-horseSound = document.querySelector('.horse');
-pigSound = document.querySelector('.pig');
-bird = document.querySelector('#bird')
-cat = document.querySelector('#cat')
-dog = document.querySelector('#dog')
-horse = document.querySelector('#horse')
-pig = document.querySelector('#pig')
-
-bird.addEventListener('click', function(){birdSound.play()})
-cat.addEventListener('click', function(){catSound.play()})
-dog.addEventListener('click', function(){dogSound.play()})
-horse.addEventListener('click', function(){horseSound.play()})
-pig.addEventListener('click', function(){pigSound.play()})
-
-document.addEventListener('keydown', animal)
-function animal(e){
-  if(e.code ==='KeyB'){
-      birdSound.play();
-  } 
-  else if(e.code === "KeyC"){
-    catSound.play();
-  }
-  else if(e.code === "KeyD"){
-    dogSound.play();
-  }
-  else if(e.code === "KeyH"){
-    horseSound.play();
-  }
-  else if(e.code === "KeyP"){
-    pigSound.play()
-  }
+function playSound(e){
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if(!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add('playing');
 }
+function removeTransition(e) {
+  if(e.propertyName != 'transform') return; 
+  this.classList.remove('playing'); 
+}
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
+
+
+
+
+
+
+
+
+
