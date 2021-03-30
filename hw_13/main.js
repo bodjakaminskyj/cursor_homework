f6b6d4f21039bdb077d339607e891b155bb5fd29
bohdan9812 <73465085+bohdan9812@users.noreply.github.com>
2021-03-30 23:35:04 +0300

    hw_13

--- hw_13/main.js
+++ hw_13/main.js
@@ -1,44 +1,85 @@
-const list = document.querySelector('ul');
+const list_characters = document.querySelector('.list_characters');
+const list_planets = document.querySelector('.list_planets');
 const info = document.querySelector('#get_info');
-const body = document.querySelector("body");
-info.addEventListener("click", getInfo)
-function getInfo(){
+const planets = document.querySelector('#get_planet');
+let page = 1;
+info.addEventListener("click", getInfo);
+planets.addEventListener("click", getPlanets);
+function getPlanets(){
+    getPagePlanets(page)
+    
+
+}
+
+async function getInfo(){
     let a = document.querySelector('#number_episode').value;
-   
     axios.get(`https://swapi.dev/api/films/${a}`)
-    .then((res)=>{
-        let people = {};
+    .then((res)=>{        
+        list_characters.innerHTML = `<h1 class="title"> Characters </h1>`;
         for (let i = 0; i < res.data.characters.length; i++) {
-            
             axios.get(res.data.characters[i]).then((res)=>{
-                people[i] = [res.data.name, res.data.birth_year, res.data.gender];
-                
-                const li = document.createElement('li');
-                li.innerHTML = people[i];
-                list.appendChild(li);
-                // const newDiv = document.createElement("div")
-               // document.body.insertAdjacentHTML('afterbegin', people[i]);
+                const character = document.createElement('div');
+                character.className = "character";
+                const img = new Image(25, 25);
                
+                
+                if(res.data.gender==='male')img.src = './images/male.png';
+                else if(res.data.gender==='female')img.src = './images/female.png';
+                else img.src = './images/none.png';
+                character.innerHTML = `
+                Name: ${res.data.name};<br>
+                Birth_year: ${res.data.birth_year};<br>
+                `
+                character.appendChild(img);
+                list_characters.appendChild(character); 
+                            
             })    
         }
     })
-    const planetsUl = document.createElement('ul');
-    body.append(planetsUl);
-    for (let i = 1; i <= 60; i++) {
-        axios.get(`https://swapi.dev/api/planets/${i}`)
+}
+async function getPagePlanets(number){
+    axios.get(`https://swapi.dev/api/planets/?page=${number}`)
     .then((res)=>{
-       
-        const planets = document.createElement('li')
-        planets.innerHTML = `${i} :
-        Name: ${res.data.name}
-        Climate: ${res.data.climate}
-        Diameter: ${res.data.diameter}
-        `;
-        
-        planetsUl.appendChild(planets);
+        list_planets.innerHTML = `<h1 class="title"> Planets </h1>`;
+        for (let i = 0; i < res.data.results.length; i++) {
+            const planet = document.createElement('div')
+            planet.className = "planet"
+            planet.innerHTML = `
+                Name: ${res.data.results[i].name}; 
+                Climate: ${res.data.results[i].climate};
+                Diameter: ${res.data.results[i].diameter}.
+                `;
+                
+        list_planets.appendChild(planet);
+        }
+        const next = document.createElement('input');
+        const back = document.createElement('input');
 
-        console.log(res.data)
+        next.id = "next";
+        next.className = "pressed-button"
+        back.id = "back";
+        back.className = "pressed-button"
+        next.setAttribute("type", "button");
+        next.setAttribute("value", "next");
+        
+        back.setAttribute("type", "button");
+        back.setAttribute("value", "back");
+        
+
+        list_planets.appendChild(next);
+        list_planets.appendChild(back);
+        next.addEventListener("click", function(){
+            page++;
+            if(page===7)page=1
+            getPagePlanets(page); 
+        });
+        back.addEventListener("click", function(){
+            page--;
+            if(page===0)page=6
+            getPagePlanets(page);
+        });
     }
         
-    )}
+)
 }
+
